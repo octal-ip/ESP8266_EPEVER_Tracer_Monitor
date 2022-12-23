5e0576c9452d6817a99f0cf9807a6cd82b02ed9e
octal-ip <hens.john@gmail.com>
2022-12-24 08:13:40 +1100

    Add files via upload

---
+++ include/secrets.h
@@ -0,0 +1,7 @@
+#define SECRET_SSID "<ssid>>"
+#define SECRET_PASS "<password>"
+#define SECRET_INFLUXDB "http://<IP address>:8086/write?db=<db name>&u=<username>&p=<password>"
+#define SECRET_INFLUX_IP_OCTET1 10
+#define SECRET_INFLUX_IP_OCTET2 1
+#define SECRET_INFLUX_IP_OCTET3 1
+#define SECRET_INFLUX_IP_OCTET4 200
